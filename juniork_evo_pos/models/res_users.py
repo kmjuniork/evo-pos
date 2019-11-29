fa4f79aa59ef517346664784042a845f8fd1cf48
Sithu Aung <sithuaung@juniork.co>
2019-11-29 10:35:52 +0000

    [IMP] juniork_evo_pos: added unique sql constraint in evopos pin [FIX] juniork_evo_pos: fix pos pin default value static

--- juniork_evo_pos/models/res_users.py
+++ juniork_evo_pos/models/res_users.py
@@ -2,7 +2,10 @@ import random
 from odoo import fields, models, api
 
 
+
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    pin_passwd = fields.Integer(string='PIN', size=6, default=random.randint(100000, 999999))
+    pin_passwd = fields.Integer(string='PIN', size=6, default=lambda self: random.randint(100000, 999999))
+
+    _sql_constraints = [('pin_passwd_unique', 'unique(pin_passwd)', 'Choose another value - it has to be unique pos pin!')]
