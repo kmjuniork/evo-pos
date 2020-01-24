2ee0f75c8e7268bcf79a138bf486350ad7b25f27
Khine Khin Mon <khinekhinmon@juniork.co>
2020-01-24 13:13:31 +0630

    [ADD] juniork_evo_pos: added top 5 product report, top selling customer report and warehouse selling report.

--- juniork_evo_pos/controllers/main.py
+++ juniork_evo_pos/controllers/main.py
@@ -31,71 +31,16 @@ def validate_token(func):
 
     return wrap
 
+
 class APIController(http.Controller):
 
-    # sales profit and loss report api
-    @validate_token
-    @http.route("/api/profit/loss/report/", type='http', auth="none", methods=['GET'], csrf=False)
-    def profit_loss_report(self, **payload):
-        data = {'used_context': {'journal_ids': False, 'state': 'posted', 'date_from': False, 'date_to': False, 'strict_range': True, 'company_id': 1, 'lang': 'en_US'}}
-        if payload.get('date_from'):
-            data['used_context']['date_from'] = payload['date_from']
-        if payload.get('date_to'):
-            data['used_context']['date_to'] = payload['date_to']
-        print(data)
-        lines = []
-        account_report = request.env['account.financial.report'].sudo().search([('id', '=', 1)])
-        child_reports = account_report.sudo()._get_children_by_order()
-        res = request.env['report.accounting_pdf_reports.report_financial'].sudo().with_context(data.get('used_context'))._compute_report_balance(child_reports)
-        for report in child_reports:
-            vals = {
-                'name': report.name,
-                'balance': res[report.id]['balance'] * report.sign,
-                'type': 'report',
-                'level': bool(report.style_overwrite) and report.style_overwrite or report.level,
-                'account_type': report.type or False, #used to underline the financial report balances
-            }
+    # common function
+    def get_filter(self, **kwargs):
 
-            lines.append(vals)
-            if report.display_detail == 'no_detail':
-                #the rest of the loop is used to display the details of the financial report, so it's not needed here.
-                continue
+        date_option = kwargs.get('date_option') if kwargs.get('date_option') else '7days'
 
-            if res[report.id].get('account'):
-                sub_lines = []
-                for account_id, value in res[report.id]['account'].items():
-                    #if there are accounts to display, we add them to the lines with a level equals to their level in
-                    #the COA + 1 (to avoid having them with a too low level that would conflicts with the level of data
-                    #financial reports for Assets, liabilities...)
-                    flag = False
-                    account = request.env['account.account'].sudo().browse(account_id)
-                    vals = {
-                        'name': account.code + ' ' + account.name,
-                        'balance': value['balance'] * report.sign or 0.0,
-                        'type': 'account',
-                        'level': report.display_detail == 'detail_with_hierarchy' and 4,
-                        'account_type': account.internal_type,
-                    }
-                    if not account.company_id.currency_id.is_zero(vals['balance']):
-                        flag = True
-                    if flag:
-                        sub_lines.append(vals)
-                lines += sorted(sub_lines, key=lambda sub_line: sub_line['name'])
-
-        return valid_response(lines)
-
-    # top 5 selling product api
-    @validate_token
-    @http.route("/api/top/selling/product", type='http', auth="none", methods=['GET'], csrf=False)
-    def top_selling_product(self, **kwargs):
-        
-        limit_value = 5
-        date_option = kwargs.get('date') if kwargs.get('date') else '7days'
-        date_selected_from = None
-        date_selected = None
-        date_selected_to = None
-
-        company_id = request.env.user.company_id.id
+        # company_id = request.env.user.company_id.id
+        company_id = 1
         warehouse_ids = request.env['stock.warehouse'].sudo().search([('company_id', '=', company_id)]).mapped('id')
         print(warehouse_ids)
         warehouse_id = kwargs.get('warehouse') if kwargs.get('warehouse') else warehouse_ids
@@ -129,8 +74,79 @@ class APIController(http.Controller):
         elif date_option == 'select_period':
             from_date = kwargs.get('from_date')
             to_date = kwargs.get('to_date')
-            date_selected_from = from_date
-            date_selected_to = to_date
+
+        data = {
+            'company_id': company_id,
+            'warehouse_id': warehouse_id,
+            'from_date': from_date,
+            'to_date': to_date
+        }
+        return data
+
+    # sales profit and loss report api
+    @validate_token
+    @http.route("/api/profit/loss/report", type='http', auth="none", methods=['GET'], csrf=False)
+    def profit_loss_report(self, **payload):
+        data = {'used_context': {'journal_ids': False, 'state': 'posted', 'date_from': False, 'date_to': False,
+                                 'strict_range': True, 'company_id': 1, 'lang': 'en_US'}}
+        if payload.get('date_from'):
+            data['used_context']['date_from'] = payload['date_from']
+        if payload.get('date_to'):
+            data['used_context']['date_to'] = payload['date_to']
+        print(data)
+        lines = []
+        account_report = request.env['account.financial.report'].sudo().search([('id', '=', 1)])
+        child_reports = account_report.sudo()._get_children_by_order()
+        res = request.env['report.accounting_pdf_reports.report_financial'].sudo().with_context(
+            data.get('used_context'))._compute_report_balance(child_reports)
+        for report in child_reports:
+            vals = {
+                'name': report.name,
+                'balance': res[report.id]['balance'] * report.sign,
+                'type': 'report',
+                'level': bool(report.style_overwrite) and report.style_overwrite or report.level,
+                'account_type': report.type or False,  # used to underline the financial report balances
+            }
+
+            lines.append(vals)
+            if report.display_detail == 'no_detail':
+                # the rest of the loop is used to display the details of the financial report, so it's not needed here.
+                continue
+
+            if res[report.id].get('account'):
+                sub_lines = []
+                for account_id, value in res[report.id]['account'].items():
+                    # if there are accounts to display, we add them to the lines with a level equals to their level in
+                    # the COA + 1 (to avoid having them with a too low level that would conflicts with the level of data
+                    # financial reports for Assets, liabilities...)
+                    flag = False
+                    account = request.env['account.account'].sudo().browse(account_id)
+                    vals = {
+                        'name': account.code + ' ' + account.name,
+                        'balance': value['balance'] * report.sign or 0.0,
+                        'type': 'account',
+                        'level': report.display_detail == 'detail_with_hierarchy' and 4,
+                        'account_type': account.internal_type,
+                    }
+                    if not account.company_id.currency_id.is_zero(vals['balance']):
+                        flag = True
+                    if flag:
+                        sub_lines.append(vals)
+                lines += sorted(sub_lines, key=lambda sub_line: sub_line['name'])
+
+        return valid_response(lines)
+
+    # top 5 selling product api
+    @validate_token
+    @http.route("/api/top/selling/product", type='http', auth="none", methods=['GET'], csrf=False)
+    def top_selling_product(self, **kwargs):
+
+        limit_value = 5
+        data = self.get_filter(**kwargs)
+        company_id = data.get('company_id')
+        warehouse_id = data.get('warehouse_id')
+        from_date = data.get('from_date')
+        to_date = data.get('to_date')
 
         order = 'desc'
         warehouse_id = str(tuple(warehouse_id)) if len(warehouse_id) > 1 else "(" + str(warehouse_id[0]) + ")"
@@ -143,15 +159,113 @@ class APIController(http.Controller):
                            so.date_order::DATE <= '%s'::DATE and
                            sl.state = 'sale' and so.company_id = %s
                            and so.warehouse_id in %s
-                           group by sl.name,pu.name order by sum %s""" % (from_date, to_date, company_id, warehouse_id, order)) + limit_clause
+                           group by sl.name,pu.name order by sum %s""" % (
+        from_date, to_date, company_id, warehouse_id, order)) + limit_clause
 
         request.cr.execute(query)
         data = request.cr.fetchall()
+        array = []
         try:
             if data:
-                return valid_response(data)
+                for arr in data:
+                    array.append({"name": arr[0], "sold-qty": arr[1], "uom": arr[2]})
+                return valid_response(array)
         except Exception as e:
-            return invalid_response('exception', e.name)    
+            return invalid_response('exception', e.name)
+
+    # top selling customer api
+    @validate_token
+    @http.route("/api/top/selling/customer", type='http', auth="none", methods=['GET'], csrf=False)
+    def top_selling_customer(self, **kwargs):
+
+        data = self.get_filter(**kwargs)
+        company_id = data.get('company_id')
+        warehouse_id = data.get('warehouse_id')
+        from_date = data.get('from_date')
+        to_date = data.get('to_date')
+
+        order = 'desc'
+        warehouse_id = str(tuple(warehouse_id)) if len(warehouse_id) > 1 else "(" + str(warehouse_id[0]) + ")"
+
+        query = ("""select cu.name,count(so.name) from sale_order so JOIN res_partner cu ON so.partner_id = cu.id where so.date_order::DATE >= '%s'::DATE and so.date_order::DATE <= '%s'::DATE and
+                           so.state = 'sale' and so.company_id = %s
+                           and so.warehouse_id in %s group by so.partner_id,cu.name order by count(so.name) %s""" % (
+            from_date, to_date, company_id, warehouse_id, order))
+
+        request.cr.execute(query)
+        data = request.cr.fetchall()
+        array = []
+        try:
+            if data:
+                for arr in data:
+                    array.append({"name": arr[0],"count": arr[1]})
+                return valid_response(array)
+        except Exception as e:
+            return invalid_response('exception', e.name)
+
+        # top selling customer api
+        # @validate_token
+        @http.route("/api/top/selling/customer", type='http', auth="none", methods=['GET'], csrf=False)
+        def top_selling_customer(self, **kwargs):
+
+            data = self.get_filter(**kwargs)
+            company_id = data.get('company_id')
+            warehouse_id = data.get('warehouse_id')
+            from_date = data.get('from_date')
+            to_date = data.get('to_date')
+
+            order = 'desc'
+            warehouse_id = str(tuple(warehouse_id)) if len(warehouse_id) > 1 else "(" + str(warehouse_id[0]) + ")"
+
+            query = ("""select cu.name,count(so.name) from sale_order so JOIN res_partner cu ON so.partner_id = cu.id where so.date_order::DATE >= '%s'::DATE and so.date_order::DATE <= '%s'::DATE and
+                               so.state = 'sale' and so.company_id = %s
+                               and so.warehouse_id in %s group by so.partner_id,cu.name order by count(so.name) %s""" % (
+                from_date, to_date, company_id, warehouse_id, order))
+
+            request.cr.execute(query)
+            data = request.cr.fetchall()
+            array = []
+            try:
+                if data:
+                    for arr in data:
+                        array.append({"name": arr[0], "count": arr[1]})
+                    return valid_response(array)
+            except Exception as e:
+                return invalid_response('exception', e.name)
+
+    # selling amount total by warehouse
+    @validate_token
+    @http.route("/api/warehouse/selling/report", type='http', auth="none", methods=['GET'], csrf=False)
+    def warehouse_selling_report(self, **kwargs):
+
+        data = self.get_filter(**kwargs)
+        company_id = data.get('company_id')
+        warehouse_id = data.get('warehouse_id')
+        from_date = data.get('from_date')
+        to_date = data.get('to_date')
+        warehouse_id = str(tuple(warehouse_id)) if len(warehouse_id) > 1 else "(" + str(warehouse_id[0]) + ")"
+
+        print('from_date', from_date)
+        print('to_date', to_date)
+
+        query = ("""select warehouse_id,sum(amount_total) from sale_order where date_order::DATE >= '%s'::DATE and date_order::DATE <= '%s'::DATE and
+                                       state = 'sale' and company_id = %s
+                                       and warehouse_id in %s group by warehouse_id""" % (
+            from_date, to_date, company_id, warehouse_id))
+
+        print(query)
+        request.cr.execute(query)
+        data = request.cr.fetchall()
+        array = []
+        try:
+            if data:
+                for arr in data:
+                    array.append({"warehouse_id": arr[0], "amount_total": arr[1]})
+                return valid_response(array)
+        except Exception as e:
+            return invalid_response('exception', e.name)
+
+
 
 
 
